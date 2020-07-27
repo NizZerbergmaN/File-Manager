5d245aa42c78e26c864b1db371843e974feb9c46
NizZerbergmaN <ivansemenov@yandex.ru>
2020-07-27 22:23:29 +0800

    Добавил модуль charder

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-### Version 2.0
+# Version 2.0
 
 
 from tkinter import *
 from tkinter.filedialog import *
+from tkinter.messagebox import *
 import fileinput
+
 
 
 def _open():
@@ -24,6 +26,15 @@ def _save():
     f.close()
 
 
+def close_win():
+    if askyesno(title="Выход", message="Вы уверены?"):
+        root.destroy()
+
+
+def about():
+    showinfo(title="Редактор", message="Простейший текстовый редактор")
+
+
 root = Tk()
 
 m = Menu(root)
@@ -33,9 +44,14 @@ fm = Menu(m)
 m.add_cascade(label="Файл", menu=fm)
 fm.add_command(label="Открыть...", command=_open)
 fm.add_command(label="Сохранить как...", command=_save)
+fm.add_command(label="Выход", command=close_win)
+
+hm = Menu(m)
+m.add_cascade(label="Справка", menu=hm)
+hm.add_command(label="О программе", command=about)
 
 txt = Text(root, width=40, height=15, font=('times', 12))
 txt.pack()
 
-txt.insert(1.0, 'Тест')
+txt.insert(1.0, "empty")
 root.mainloop()
